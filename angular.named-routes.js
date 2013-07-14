186c318f423d91cc3cf5920af8e77bcfb6cbfc01
Zenobius Jiricek <zenobius.jiricek@gmail.com>
2013-07-14 16:31:22 +0930

    Update angular.named-routes.js

--- angular.named-routes.js
+++ angular.named-routes.js
@@ -1,12 +1,16 @@
-angular.module("zj.namedRoutes", []).
-    factory("$NamedRouteService", function($rootScope, $route, $Application, $log){
+angular.module("zj.namedRoutes", [])
+    // as a non standard way to fix html5Mode
+    // set this to something like :
+    // .constant("NamedRoutesPrefix", "#!")
+    .constant("NamedRoutesPrefix", "")
+    .factory("$NamedRouteService", function($rootScope, $route, $location, $Application, $log, NamedRoutesPrefix){
         var routeService = {
             reverse: function (routeName, options) {
-                /*  1. Step through routes,
-                        build list of matching routes,
-                    2. replace placeholders with supplied arguments,
-                    3. return built url.
-                */
+                    /* Step through routes,
+                        pick one with matching name,
+                        replace placeholders with supplied arguments,
+                        return built url.
+                    */
                     var routes = routeService.match(routeName);
                     if (routes.length == 1) {
                         return routeService.resolve(options, routes[0]);
@@ -17,7 +21,6 @@ angular.module("zj.namedRoutes", []).
                 },
 
             match: function(routeName){
-                // 1. pick matching routes
                     routes = []
                     angular.forEach($route.routes,function (config,route) {
                         if(config.name===routeName){
@@ -27,7 +30,6 @@ angular.module("zj.namedRoutes", []).
                     return routes;
                 },
             resolve: function(options, route) {
-                // 2. replace placholders with supplied arguments
                     var parts = route.split('/');
                     for (var i = 0; i < parts.length; i++) {
                         var part = parts[i];
@@ -36,46 +38,49 @@ angular.module("zj.namedRoutes", []).
                             if (parts[i] == undefined) throw Error('Attribute \'' + part + '\' was not given for route \'' + route + '\'')
                         }
                     }
-                    // TODO: instead of hardcoding the hash here, workout a way to do
-                    //       this based on the application config.
-                    return "#" + parts.join('/');
+                    var output = parts.join('/');
+                    return NamedRoutesPrefix+output
                 }
             }
 
         return routeService;
-    }).
+    })
 
-    directive('zjNamedRoute', function($log, $NamedRouteService){
-            /*  Given that the following route exists :
+    .directive('namedUrl', function($log, $NamedRouteService){
+            /* Given that the following route exists :
                     .when('/products/:cat/:id', {
                         controller: 'OptionalController',
                         template: '/static/javascripts/application/templates/optional-template.html',
                         name: 'item-detail'
                     })
 
-                And that an element is present :
-                    <a data-zj-named-route='item-detail'
-                    data-zj-named-kwargs='{pk: 1, category:"fish"}' # or
-                    data-zj-named-args='[1, "fish"]'
-                    data-zj-named-target='href'>Salmon Info</a>
+               And that an element is present :
+                 <a data-named-url='item-detail'
+                 data-named-args='["fish", 1]'
+                 data-named-target='href'>Salmon Info</a>
 
-                Return the following
-                    <a href="#/products/fish/1/">Salmon Info</a>
+                 return the following
+                 <a href="#/products/fish/1/">Salmon Info</a>
              */
             return {
                 restrict: "AC",
                 link: function(scope, element, attributes){
-                    var targetAttribute = 'zjNamedTarget' in attributes?attributes.zjNamedTarget:'href'
-                    element.attr(targetAttribute,
-                        $NamedRouteService.reverse(attributes.zjNamedRoute, attributes.zjNamedArgs))
+                    var options = {};
+                    var newKey;
+                    for(var key in attributes){
+                        if(key.indexOf(kwarg)==0){
+                            newKey = key.replace('kwarg','');
+                            newKey = newKey.charAt(0).toUpperCase() + newKey.slice(1);
+                            options[newKey] = attributes[key];
+                        }
+                    }
+                    var url = $NamedRouteService.reverse(attributes.namedUrl, options)
+                    element.attr('href', url)
                 }
             }
-        }).
+        })
 
-    filter('resolveroute', function ($route, $NamedRouteService) {
-            /*
-                <a data-ng-href="'something'|resolveroute">Something</a>
-             */
+    .filter('url', function ($route, $NamedRouteService) {
             return function(input, options){
                 return $NamedRouteService.reverse(input, options);
             };
