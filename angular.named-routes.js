a66431691cb664656790d9c201b3b81f8bef8e62
Damian Dlugosz <d.dlugosz@bestnetwork.it>
2014-03-08 16:24:51 +0100

    Directive ngRoute Directive ngLink Filter route

--- angular.named-routes.js
+++ angular.named-routes.js
@@ -1,90 +1,142 @@
 "use strict";
 
-angular.module("zj.namedRoutes", [])
-    // as a non standard way to fix html5Mode
-    // set this to something like :
-    // .constant("NamedRoutesPrefix", "#!")
-    .constant("NamedRoutesPrefix", "")
-    .factory("$NamedRouteService", function($rootScope, $route, $location, $log, NamedRoutesPrefix){
-        var routeService = {
-            reverse: function (routeName, options) {
-                    /* Step through routes,
-                        pick one with matching name,
-                        replace placeholders with supplied arguments,
-                        return built url.
-                    */
-                    var routes = routeService.match(routeName);
-                    if (routes.length == 1) {
-                        return routeService.resolve(options, routes[0]);
-                    }else if (routes.length === 0) {
-                        throw new Error('Route ' + routeName + ' not found');
-                    }
-                    throw new Error('Multiple routes matching ' + routeName + ' were found');
-                },
+// Angular Named Routes
+// https://github.com/airtonix/angular-named-routes
+// https://github.com/airtonix/angular-named-routes/blob/master/angular.named-routes.js
 
-            match: function(routeName){
-                    var routes = [];
-                    angular.forEach($route.routes,function (config,route) {
-                        if(config.name===routeName){
-                            routes.push(route);
-                        }
-                    });
-                    return routes;
-                },
-            resolve: function(options, route) {
-                    var parts = route.split('/');
-                    for (var i = 0; i < parts.length; i++) {
-                        var part = parts[i];
-                        if (part[0] === ':') {
-                            parts[i] = options[part.replace(':', '')];
-                            if (parts[i] === undefined) throw new Error('Attribute \'' + part + '\' was not given for route \'' + route + '\'');
-                        }
-                    }
-                    var output = parts.join('/');
-                    return NamedRoutesPrefix+output;
-                }
-            };
+var zj = angular.module("zj.namedRoutes", []);
 
-        return routeService;
-    })
+zj.config(function ($locationProvider) {
+    app.constant("NamedRoutesPrefix", ($locationProvider.html5Mode() ? '' : '#'));
+});
 
-    .directive('namedUrl', function($log, $NamedRouteService){
-            /* Given that the following route exists :
-                    .when('/products/:cat/:id', {
-                        controller: 'OptionalController',
-                        template: '/static/javascripts/application/templates/optional-template.html',
-                        name: 'item-detail'
-                    })
-
-               And that an element is present :
-                 <a data-named-url='item-detail'
-                 data-named-args='["fish", 1]'
-                 data-named-target='href'>Salmon Info</a>
-
-                 return the following
-                 <a href="#/products/fish/1/">Salmon Info</a>
+zj.factory("$NamedRouteService", function($rootScope, $route, $location, NamedRoutesPrefix){
+    return {
+        reverse: function (routeName, options) {
+            /* Step through routes,
+             pick one with matching name,
+             replace placeholders with supplied arguments,
+             return built url.
              */
-            return {
-                restrict: "AC",
-                link: function(scope, element, attributes){
-                    var options = {};
-                    var newKey;
-                    for(var key in attributes){
-                        if(key.indexOf('kwarg')===0){
-                            newKey = key.replace('kwarg','');
-                            newKey = newKey.charAt(0).toLowerCase() + newKey.slice(1);
-                            console.log(key, newKey)
-                            options[newKey] = attributes[key];
-                        }
-                    }
-                    var url = $NamedRouteService.reverse(attributes.namedUrl, options);
-                    element.attr('href', url);
-                }
-            };
-        })
+            var routes = this.match(routeName);
+            if (routes.length == 1) {
+                return this.resolve(routes[0], options);
+            }else if (routes.length === 0) {
+                throw new Error('Route ' + routeName + ' not found');
+            }
+            throw new Error('Multiple routes matching ' + routeName + ' were found');
+        },
 
-    .filter('url', function ($route, $NamedRouteService) {
-            return function(input, options){
-                return $NamedRouteService.reverse(input, options);
-            };
-        });
+        match: function(routeName){
+            var routes = [];
+            angular.forEach($route.routes, function (config, route) {
+                if(config.name === routeName){
+                    routes.push(route);
+                }
+            });
+            return routes;
+        },
+
+        resolve: function(route, options) {
+            var parts = route.split('/');
+            var attributes = angular.copy(options);
+            var required = '';
+            var extra = '';
+
+            // Required attributes
+            for (var i in parts) {
+                var part = parts[i];
+                if (part[0] === ':') {
+                    var name = part.replace(':', '');
+                    if (attributes[name] === undefined) throw new Error('Attribute \'' + name + '\' was not given for route \'' + route + '\'');
+                    parts[i] = encodeURIComponent(attributes[name]);
+                    delete attributes[name];
+                }
+            }
+            required += parts.join('/');
+
+            // Extra attributes
+            if (attributes) {
+                for (var key in attributes) {
+                    extra += key + '=' + encodeURIComponent(attributes[key]).replace(/%20/g, '+') + '#';
+                }
+            }
+
+            return NamedRoutesPrefix + required + (extra ? '?' + extra : '');
+        }
+    };
+});
+
+/* Usage example:
+ <a ng-route="item-detail">Details</a>
+ */
+zj.directive('ngRoute', function($NamedRouteService) {
+    return {
+        restrict: "A",
+        link: function(scope, element, attributes){
+            var url = $NamedRouteService.reverse(attributes.ngRoute, {});
+            element.attr('href', url);
+        }
+    };
+});
+
+/* Usage example:
+ <a ng-link route-name="item-detail" route-params="{id:13}">Details</a>
+ <button ng-link route-name="submit">Submit</button>
+ <ngLink route-name="home">Home</ngLink>
+ */
+zj.directive('ngLink', function($NamedRouteService, $window) {
+    function updateLink(element, routeName, params) {
+        var url = $NamedRouteService.reverse(routeName, params);
+        var tagName = element[0].nodeName;
+
+        switch (tagName) {
+            case 'A':
+            case 'NG-LINK':
+                element.attr('href', url);
+                break;
+            default:
+                element.unbind('click').click( function(event) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    $window.location = url;
+                });
+        }
+    };
+
+    return {
+        restrict: "AE",
+        scope: {
+            params: '=routeParams'
+        },
+        replace: true,
+        transclude: true,
+        template: function(tElement) {
+            var tagName = tElement[0].nodeName;
+            if (tElement[0].nodeName == 'NG-LINK') tagName = 'A';
+            return '<'+tagName+' ng-transclude></'+tagName+'>';
+        },
+        link: function(scope, element, attributes) {
+            if (attributes.routeParams) {
+                scope.$watch('params', function(value) {
+                    if (value != null && typeof value === 'object') {
+                        for (var key in value) if (value[key] === undefined) return;
+                        updateLink(element, attributes.routeName, value);
+                    }
+                });
+            } else {
+                updateLink(element, attributes.routeName, {});
+            }
+        }
+    };
+});
+
+/* Usage example:
+ <a href="[[ 'item-detail' | route:{id:13} ]]">Details</a>
+ <form action="[[ 'item-detail' | route:{id:13} ]]">
+ */
+zj.filter('route', function ($NamedRouteService) {
+    return function(input, options){
+        return $NamedRouteService.reverse(input, options);
+    };
+});
